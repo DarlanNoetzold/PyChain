54a71347fdfbfc37688787e66bb26300f8f10421
Darlan Noetzold <darlan.noetzold@gmail.com>
2022-01-24 06:37:33 -0300

    .

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import hashlib
 import json
+from textwrap import dedent
 from time import time
+from uuid import uuid4
+
+from flask import Flask, jsonify
 
 
 class Blockchain(object):
@@ -100,3 +104,35 @@ y = 0  # We don't know what y should be yet...
 while sha256(f'{x*y}'.encode()).hexdigest()[-1] != "0":
     y += 1
 print(f'The solution is y = {y}')
+
+# Instantiate our Node
+app = Flask(__name__)
+
+# Generate a globally unique address for this node
+node_identifier = str(uuid4()).replace('-', '')
+
+# Instantiate the Blockchain
+blockchain = Blockchain()
+
+
+@app.route('/mine', methods=['GET'])
+def mine():
+    return "We'll mine a new Block"
+
+
+@app.route('/transactions/new', methods=['POST'])
+def new_transaction():
+    return "We'll add a new transaction"
+
+
+@app.route('/chain', methods=['GET'])
+def full_chain():
+    response = {
+        'chain': blockchain.chain,
+        'length': len(blockchain.chain),
+    }
+    return jsonify(response), 200
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
