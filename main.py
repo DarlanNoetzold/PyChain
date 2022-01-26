1db1fcc568bc91018b50258b5caad8b84590189c
Darlan Noetzold <darlan.noetzold@gmail.com>
2022-01-26 06:53:36 -0300

    Update main.py

--- main.py
+++ main.py
@@ -134,7 +134,7 @@ def full_chain():
     return jsonify(response), 200
 
 @app.route('/transactions/new', methods=['POST'])
-def new_transaction():
+def new_transactionAs():
     values = request.get_json()
 
     # Check that the required fields are in the POST'ed data
@@ -148,6 +148,34 @@ def new_transaction():
     response = {'message': f'Transaction will be added to Block {index}'}
     return jsonify(response), 201
 
+
+@app.route('/mine', methods=['GET'])
+def mineA():
+    # We run the proof of work algorithm to get the next proof...
+    last_block = blockchain.last_block
+    last_proof = last_block['proof']
+    proof = blockchain.proof_of_work(last_proof)
+
+    # We must receive a reward for finding the proof.
+    # The sender is "0" to signify that this node has mined a new coin.
+    blockchain.new_transaction(
+        sender="0",
+        recipient=node_identifier,
+        amount=1,
+    )
+
+    # Forge the new Block by adding it to the chain
+    previous_hash = blockchain.hash(last_block)
+    block = blockchain.new_block(proof, previous_hash)
+
+    response = {
+        'message': "New Block Forged",
+        'index': block['index'],
+        'transactions': block['transactions'],
+        'proof': block['proof'],
+        'previous_hash': block['previous_hash'],
+    }
+    return jsonify(response), 200
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
